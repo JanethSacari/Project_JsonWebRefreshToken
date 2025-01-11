e8cc30a8baf537f4ee452bb876d3cb8242b2ce69
Janeth Mamani Sacari <janeth.mamani.sacari@gmail.com>
2025-01-10 23:58:49 -0300

    Project RepliforceJsonValidator - Starting project

---
+++ src/main/java/io/repliforce/RepliforceJsonValidator/RepliforceJsonValidatorApplication.java
@@ -0,0 +1,13 @@
+package io.repliforce.RepliforceJsonValidator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RepliforceJsonValidatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RepliforceJsonValidatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/repliforce/RepliforceJsonValidator/RepliforceJsonValidatorApplicationTests.java
@@ -0,0 +1,13 @@
+package io.repliforce.RepliforceJsonValidator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RepliforceJsonValidatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
