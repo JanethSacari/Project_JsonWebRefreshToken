d89ba60c0ea9a63312c14f0e9e686accb30a2743
Janeth Mamani Sacari <janeth.mamani.sacari@gmail.com>
2025-01-16 23:34:41 -0300

    Project_RepliforceJsonValidator - Role entity created.

--- src/main/java/io/repliforce/RepliforceJsonValidator/domain/User.java
+++ src/main/java/io/repliforce/RepliforceJsonValidator/domain/User.java
@@ -8,7 +8,6 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.management.relation.Role;
 import java.util.ArrayList;
 import java.util.Collection;
 
