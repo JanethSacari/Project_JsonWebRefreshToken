fb6f950c68c00d482c467713a3438e26137b00e2
Janeth Mamani Sacari <janeth.mamani.sacari@gmail.com>
2025-01-30 23:52:45 -0300

    Project_RepliforceJsonValidator - Starting Authentication filter implementation

--- src/main/java/io/repliforce/RepliforceJsonValidator/security/SecurityConfig.java
+++ src/main/java/io/repliforce/RepliforceJsonValidator/security/SecurityConfig.java
@@ -1,5 +1,6 @@
 package io.repliforce.RepliforceJsonValidator.security;
 
+import io.repliforce.RepliforceJsonValidator.filter.CustomAuthenticationFilter;
 import lombok.AllArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
@@ -8,6 +9,8 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import static org.springframework.security.config.http.SessionCreationPolicy.STATELESS;
 
 @Configuration
 @EnableWebSecurity
@@ -23,6 +26,9 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        super.configure(http);
+        http.csrf().disable();
+        http.sessionManagement().sessionCreationPolicy(STATELESS);
+        http.authorizeRequests().anyRequest().permitAll();
+        http.addFilter(null);
     }
 }
