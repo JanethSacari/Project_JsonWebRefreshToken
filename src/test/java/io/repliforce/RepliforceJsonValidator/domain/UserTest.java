f76e56bbf16b536e3fddf7b308998aa2710a9d7c
Janeth Mamani Sacari <janeth.mamani.sacari@gmail.com>
2025-03-09 19:52:02 -0300

    Project_RepliforceJsonValidator - Creating User method test

--- src/test/java/io/repliforce/RepliforceJsonValidator/domain/UserTest.java
+++ src/test/java/io/repliforce/RepliforceJsonValidator/domain/UserTest.java
@@ -1,7 +1,14 @@
 package io.repliforce.RepliforceJsonValidator.domain;
 
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class UserTest {
+
+    @Test
+    public void createUserSuccefully (){
+
+    }
 
 }
