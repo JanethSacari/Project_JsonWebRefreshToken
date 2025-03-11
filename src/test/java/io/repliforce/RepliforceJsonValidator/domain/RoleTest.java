7f32eb2f2272b21405dea14cfef90a150c2d9606
Janeth Mamani Sacari <janeth.mamani.sacari@gmail.com>
2025-03-11 20:56:57 -0300

    Project_RepliforceJsonValidator - Implementing RoleTest

--- src/test/java/io/repliforce/RepliforceJsonValidator/domain/RoleTest.java
+++ src/test/java/io/repliforce/RepliforceJsonValidator/domain/RoleTest.java
@@ -1,7 +1,37 @@
 package io.repliforce.RepliforceJsonValidator.domain;
 
+import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
-class RoleTest {
+public class RoleTest {
 
+    @Test
+    public void testRoleCreation() {
+        Role role = new Role(1L, "ROLE_ADMIN");
+
+        assertEquals(1L, role.getId());
+        assertEquals("ROLE_ADMIN", role.getRolename());
+    }
+
+    @Test
+    public void testNoArgsConstructor() {
+        Role role = new Role();
+
+        assertNull(role.getId());
+        assertNull(role.getRolename());
+
+        role.setId(2L);
+        role.setRolename("ROLE_USER");
+
+        assertEquals(2L, role.getId());
+        assertEquals("ROLE_USER", role.getRolename());
+    }
+
+    @Test
+    public void testAllArgsConstructor() {
+        Role role = new Role(3L, "ROLE_MANAGER");
+
+        assertEquals(3L, role.getId());
+        assertEquals("ROLE_MANAGER", role.getRolename());
+    }
 }
